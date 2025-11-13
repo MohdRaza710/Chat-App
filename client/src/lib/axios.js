e6c80e36390eec9943e31393ac4884beb02f9b5a
MohdRaza710 <mrmraza.710@gmail.com>
2025-11-13 10:47:35 +0300

    changes

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "https://chat-app-client-d11t.onrender.com" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "https://chat-app-server-5hcy.onrender.com" : "/api",
     withCredentials: true,
 });
