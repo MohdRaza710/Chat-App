028ed3a48d235dd9883a966f543583119f680f38
MohdRaza710 <mrmraza.710@gmail.com>
2025-11-13 10:09:04 +0300

    changes

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -35,7 +35,7 @@ function ChatContainer() {
   return (
     <>
       <ChatHeader />
-      <div className="flex-1 px-6 overflow-y-auto py-8 w-fit">
+      <div className="flex-1 overflow-y-auto p-8 ">
         {messages.length > 0 && !isMessagesLoading ? (
           <div className="max-w-3xl mx-auto space-y-6">
             {messages.map((msg) => (
@@ -44,22 +44,31 @@ function ChatContainer() {
                 className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}
               >
                 <div
-                  className={`chat-bubble relative ${msg.senderId === authUser._id
-                      ? "bg-cyan-600 text-white"
-                      : "bg-slate-800 text-slate-200"
-                    }`}
+                  className={`flex mb-2 ${msg.senderId === authUser._id ? "justify-end" : "justify-start"}`}
                 >
-                  {msg.image && (
-                    <img src={msg.image} alt="Shared" className="rounded-lg h-48 object-cover" />
-                  )}
-                  {msg.text && <p className="mt-2">{msg.text}</p>}
-                  <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
-                    {new Date(msg.createdAt).toLocaleTimeString(undefined, {
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    })}
-                  </p>
+                  <div
+                    className={`chat-bubble relative ${msg.senderId === authUser._id
+                      ? "bg-cyan-600 text-white p-2 w-fit rounded-lg"
+                      : "bg-slate-800 text-slate-200 p-2 w-fit rounded-lg"
+                      }`}
+                  >
+                    {msg.image && (
+                      <img
+                        src={msg.image}
+                        alt="Shared"
+                        className="rounded-lg h-48 object-cover"
+                      />
+                    )}
+                    {msg.text && <p className="mt-2">{msg.text}</p>}
+                    <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
+                      {new Date(msg.createdAt).toLocaleTimeString(undefined, {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}
+                    </p>
+                  </div>
                 </div>
+
               </div>
             ))}
             {/* 👇 scroll target */}
